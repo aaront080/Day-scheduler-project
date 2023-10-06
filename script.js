91077982bf3d4743d23411aacbeb4f6959c6afc1
Aaron Torres <aarontorres@Aarons-MBP.lan>
2023-10-05 19:07:22 -0700

    js notes added

--- script.js
+++ script.js
@@ -1,68 +1,61 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
 $(document).ready(function(){  //jQuery call to ensure html and css are "ready" before bootstrap functions are executed.
 
-  // TODO: Add code to display the current date in the header of the page.
+
 
 function clock(){
-var todayDate = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a'); //day.js calender date and real time updating clock
+var todayDate = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a'); /*day.js calender date and real time updating clock */
 $('#currentDateTime').text(todayDate);}
 
 setInterval(clock, 1000)
 
 
 
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
 
-$(".saveBtn").on("click", function() {  
+$(".saveBtn").on("click", function() {    //click event triggers function to log user input.
 
-  console.log(this);
-  var userInput = $(this).siblings(".userInput").val()
-  var hourNum = $(this).parent().attr("id");
+  console.log(this);                         
+  var userInput = $(this).siblings(".userInput").val()  //captures value of user input
+  var hourNum = $(this).parent().attr("id");         //captures the time hour block user input was entered in
 
-  localStorage.setItem(hourNum, userInput)
+  localStorage.setItem(hourNum, userInput)  //stores info to local storage
 })
 
   
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
 
     function scheduleTracker() {
-     var currentHour = parseInt(dayjs().format('H'));
+     var currentHour = parseInt(dayjs().format('H'));  //the current hour of day in 24 hour format
 
       $(".time-block").each(function() {
-       var hourBlock = parseInt($(this).attr("id"));
-        console.log(typeof hourBlock)
-        console.log(typeof currentHour)
+       var hourBlock = parseInt($(this).attr("id"));  //the ID of each time block converted into a number 
+        //console.log(typeof hourBlock) 
+        //console.log(typeof currentHour)
         console.log(currentHour, hourBlock)
 
-        if (currentHour === hourBlock) {
-          $(this).addClass(".present");
-          $(this).removeClass(".past");
-          $(this).removeClass(".future")
+        if (currentHour === hourBlock) {       //if statements that will apply different classes depending on
+          $(this).addClass("present");         //what the current hour of the day it is (in 24 hour format)
+          $(this).removeClass("past");
+          $(this).removeClass("future")
+        } else if (currentHour < hourBlock) {
+          $(this).addClass("future");
+          $(this).removeClass("past");
+          $(this).removeClass("present")
+        } else {
+          $(this).addClass("past");
+          $(this).removeClass("present");
+          $(this).removeClass("future")
         }
     })
    } 
 
+   scheduleTracker()
+
     //console.log(currentHour)
     //console.log(hourBlock)
 
-    console.log(scheduleTracker())
-  
 
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
+  
+=
     
     $("#9 .userInput").val(localStorage.getItem("9"));
     $("#10 .userInput").val(localStorage.getItem("10"));
